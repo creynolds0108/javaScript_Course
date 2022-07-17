cdc07373289254366bc6440f776642f7be07f7f7
Chad Reynolds <c.reynolds0108@proton.me>
2022-07-16 22:09:01 -0400

    Edited app.js file

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
-console.log('Hello World!');
-console.log('This guy is going way to fast');
+//var, let, const
+// var name='John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// Init var
+var greeting;
+greeting = 'Hello';
+console.log(greeting);
+
+// variable can include letters, numbers, _ , $
+// cannot start with a number
+
+var $name='John Doe'
+console.log($name);
+
+//Multi word variables
+var firstName = 'John'; // Camel Case
+var first_name= 'Sarah'; // Underscore
+var FirstName = 'tom'; // Pascal case
+
+// let 
+// let name='John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// const - constant can't be changed
+// have to have a value assigned
+
+// const name = 'John';
+// console.log(name);
+
+const person = {
+  name:'john'
+}
+person.name = "Sara"
+person.age = '40'
+console.log(person);
